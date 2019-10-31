d7dba65bf86fbfe4d11af4089305dae48cfc4c90
Mustapha-Belkacim <mustapha.belkacim@gmail.com>
2019-10-31 21:49:05 +0100

    Create GroupSerializers

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -7,3 +7,9 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 		model = User
 		fields = ['url', 'username', 'email', 'groups']
 
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Group
+		fields = ['url', 'name']
+		
