b413456ccf5cc9bfc9dd053e7873a432e021fec2
Mustapha-Belkacim <mustapha.belkacim@gmail.com>
2019-10-31 21:49:46 +0100

    Create UserViewSet

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
 
-# Create your views here.
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+	"""API endpoint that allows users to be viewed and edited"""
+	queryset = User.objects.all().order_by('-date_joined')
+	serializer_class = UserSerializer
+
